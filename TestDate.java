51ce2b648c0459b534b98c3819e28c9647cb8291
Thibault Bardinet-Langlois <langlois.bardinet.thibault@gmail.com>
2022-06-07 01:00:00 +0200

    commit

--- TestDate.java
+++ TestDate.java
@@ -1,4 +1,5 @@
 import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
@@ -6,8 +7,7 @@ import java.util.Scanner;
 
 class TestDate {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {  
         Scanner sc = new Scanner(System.in);
         System.out.println("Please fill a day (1-31) :");
         int day = sc.nextInt();
@@ -15,9 +15,15 @@ class TestDate {
         int month = sc.nextInt();
         System.out.println("Please fill a year :");
         int year = sc.nextInt();
-        sc.close();
-
-        // TODO : your code after this line
         
+        Calendar c = new GregorianCalendar(year, month - 1, day);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Date d = c.getTime();
+        int dayOfTheWeek = c.get(Calendar.DAY_OF_WEEK);
+
+        
+        System.out.println("The date is : " + sdf.format(d));
+        System.out.println("The day of the week is : " + dayOfTheWeek);
+        sc.close();
     }
 }
